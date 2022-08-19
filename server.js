bebdbb4a02526af8af2b13f3d3ee070a67542522
Rakshit Dabral <96056758+rakshitdabral@users.noreply.github.com>
2022-08-19 18:59:59 +0530

    version 2.0.0

--- server.js
+++ server.js
@@ -1,44 +1,51 @@
-const path = require('path');
-const express=require('express')
-const https = require('https')
+const path = require("path");
+const express = require("express");
+const https = require("https");
 
-var fs = require('fs');
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 
-const app= express()
+const app = express();
 const port = 3000;
-app.set('view engine', 'ejs');
-app.use(express.static('public'))
-app.use(express.static('src'));
+
+app.use(express.static("public"));
+
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
-app.get('/',function(req,res){
-    res.sendFile(__dirname+"/index.html")
-})
+app.get("/", function (req, res) {
+  res.sendFile(__dirname + "/index.html");
+});
 
-app.post('/',function(req,res) {
-    const query=req.body.cityName;
-    const apiKey="8ef0a674eb5df59d317dd49f4d4f4249";
-    const units="metric"
-    const url="https://api.openweathermap.org/data/2.5/weather?q="+query+"&appid=8ef0a674eb5df59d317dd49f4d4f4249&units=metric"
-    https.get(url,function(response){
-        console.log(response.statusCode)
+app.post("/", function (req, res) {
+  const query = req.body.cityName;
+  const apiKey = "8ef0a674eb5df59d317dd49f4d4f4249";
+  const units = "metric";
+  const url =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    query +
+    "&appid=8ef0a674eb5df59d317dd49f4d4f4249&units=metric";
+  https.get(url, function (response) {
+    if(response.statusCode === 200) {
+        response.on("data", (chunk) => {
+            JSON.parse(chunk);
+            const weatherData = JSON.parse(chunk);
+            const temp = weatherData.main.temp;
+            const weatherDesc = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+            const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+            const humidity = weatherData.main.humidity;
+            const speed = weatherData.wind.speed;
+      
+            res.render("value", { cityName: query , specificTemp:temp,desc:weatherDesc,weatherIcon:icon,specificHumidity:humidity,specificSpeed:speed });
+          });
+    }
+    else{
+        res.sendFile(__dirname+"/index.html");
+    }
+    
+  });
+});
 
-        response.on('data', (chunk) => {
-            JSON.parse(chunk)
-            const weatherData = JSON.parse(chunk)
-            const temp=weatherData.main.temp;
-            const weatherDesc=weatherData.weather[0].description
-            const icon=weatherData.weather[0].icon
-            const imageURL="http://openweathermap.org/img/wn/" +icon+"@2x.png";
-            
-        })
-        
-    })
-})
-
-
-
-app.listen(port, function (){
-    console.log("server running on port " + port)
-})
+app.listen(port, function () {
+  console.log("server running on port " + port);
+});
